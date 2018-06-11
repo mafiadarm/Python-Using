10d44468996c10aa66601f890abba9ca85c1c7a2
Loffew <Loffew@users.noreply.github.com>
2018-06-11 09:23:49 +0800

    ...

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
+---
+运用在生产环境的实例
+---
+
 # Python-Using
+
+check_table_project
+
+​	人资使用，打卡机导出打卡资料，对比员工信息及上下班信息等，汇成最终的excel报表
+
+copy_data_backup
+
+​	服务器数据库备份定期复制到其他网络映射盘
+
+e_mail_send_payroll
+
+​	人资使用，根据员工邮箱信息，批量通过邮件发工资条
+
+data_views
+
+​	数据可视化的尝试性使用，对公司数据进行分析
+
+
+
+dataBase_Form_import_export.py
+
+​	sqlserver的python包的尝试性使用
+
+ListeRaid.py
+
+​	监控raid状态
+
+Single_Process_copy_data.py
+
+​	早期使用的单线程拷贝文件到指定位置
+
+threading_pool_copy_data.py
+
+​	多线程拷贝文件到指定位置，受制于内存大小
+
+split_copy_data.py
+
+​	优化后的多线程拷贝文件，把大小文件分开，小文件使用多线程，大文件使用单线程，保证同等内存大小的情况下，能最优化的运行
--- dataBase_Form_import_export.py
+++ dataBase_Form_import_export.py
@@ -5,6 +5,7 @@ import os
 import pymssql
 import sys
 
+
 class MSSQLHelper:
     """
     对pymssql的简单封装
@@ -15,7 +16,7 @@ class MSSQLHelper:
 
     """
 
-    def __init__(self,host,user,pwd,db):
+    def __init__(self, host, user, pwd, db):
         self.host = host
         self.user = user
         self.pwd = pwd
@@ -27,15 +28,15 @@ class MSSQLHelper:
         返回: conn.cursor()
         """
         if not self.db:
-            raise(NameError,"没有设置数据库信息")
-        self.conn =pymssql.connect(host=self.host,user=self.user,password=self.pwd,database=self.db,charset="utf8")
+            raise (NameError, "没有设置数据库信息")
+        self.conn = pymssql.connect(host=self.host, user=self.user, password=self.pwd, database=self.db, charset="utf8")
         cur = self.conn.cursor()
         if not cur:
-            raise(NameError,"连接数据库失败")
+            raise (NameError, "连接数据库失败")
         else:
             return cur
 
-    def ExecQuery(self,sql):
+    def ExecQuery(self, sql):
         """
         执行查询语句
         返回的是一个包含tuple的list，list的元素是记录行，tuple的元素是每行记录的字段
@@ -48,13 +49,13 @@ class MSSQLHelper:
         """
         cur = self.__GetConnect()
         cur.execute(sql.encode("utf8"))
-        resList =  cur.fetchall()
+        resList = cur.fetchall()
 
-        #查询完毕后必须关闭连接
+        # 查询完毕后必须关闭连接
         self.conn.close()
         return resList
 
-    def ExecNonQuery(self,sql):
+    def ExecNonQuery(self, sql):
         """
         执行非查询语句
 
@@ -70,11 +71,11 @@ class MSSQLHelper:
         self.conn.close()
 
 
-def RemoveDirectory (top):
+def RemoveDirectory(top):
     while 1:
         if os.path.exists(top):
             if len(os.listdir(top)) == 0:
-                os.rmdir (top)
+                os.rmdir(top)
                 break
             else:
                 for root, dirs, files in os.walk(top, topdown=False):
@@ -85,53 +86,55 @@ def RemoveDirectory (top):
         else:
             break
 
-def export(db,user,password):
+
+def export(db, user, password):
     """导出数据库"""
-    #得到当前脚本的执行目录
+    # 得到当前脚本的执行目录
     currentDirectory = os.getcwd()
 
-    #查看是否已经存在备份目录，如果有则删除，没有则新建目录
-    backUpDirectory = "%s\\%s" %( currentDirectory,db+"Backup")
+    # 查看是否已经存在备份目录，如果有则删除，没有则新建目录
+    backUpDirectory = "%s\\%s" % (currentDirectory, db + "Backup")
     if os.path.exists(backUpDirectory):
         RemoveDirectory(backUpDirectory)
         os.mkdir(backUpDirectory)
     else:
         os.mkdir(backUpDirectory)
 
-    #得到要到处的数据库的所有表
-    ms = MSSQLHelper(host="localhost",user=user,pwd=password,db=db)
+    # 得到要到处的数据库的所有表
+    ms = MSSQLHelper(host="localhost", user=user, pwd=password, db=db)
     for (name,) in ms.ExecQuery("select name from sysobjects where xtype='U'"):
-        currentTablePath = "%s\\%s.txt"%(backUpDirectory,name)
-        r = os.popen('BCP %s..%s out %s -c -U"%s" -P"%s"' % (db,name,currentTablePath,user,password))
-        print (r.read())
+        currentTablePath = "%s\\%s.txt" % (backUpDirectory, name)
+        r = os.popen('BCP %s..%s out %s -c -U"%s" -P"%s"' % (db, name, currentTablePath, user, password))
+        print(r.read())
         r.close()
 
-def inport(db,user,password):
+
+def inport(db, user, password):
     """导入数据库"""
-    #得到当前脚本的执行目录
+    # 得到当前脚本的执行目录
     currentDirectory = os.getcwd()
 
-    #查看是否已经存在备份目录，如果有则执行
-    backUpDirectory = "%s\\%s" %( currentDirectory,db+"Backup")
+    # 查看是否已经存在备份目录，如果有则执行
+    backUpDirectory = "%s\\%s" % (currentDirectory, db + "Backup")
     if os.path.exists(backUpDirectory):
-        #得到要到处的数据库的所有表
-        ms = MSSQLHelper(host="localhost",user=user,pwd=password,db=db)
+        # 得到要到处的数据库的所有表
+        ms = MSSQLHelper(host="localhost", user=user, pwd=password, db=db)
         for (name,) in ms.ExecQuery("select name from sysobjects where xtype='U'"):
-            currentTablePath = "%s\\%s.txt"%(backUpDirectory,name)
-            r = os.popen('BCP %s..%s in %s -c -U"%s" -P"%s"' % (db,name,currentTablePath,user,password))
-            print (r.read())
+            currentTablePath = "%s\\%s.txt" % (backUpDirectory, name)
+            r = os.popen('BCP %s..%s in %s -c -U"%s" -P"%s"' % (db, name, currentTablePath, user, password))
+            print(r.read())
             r.close()
 
 
 def main():
-
     db = "ROSUNHB"
     user = "sa"
     password = ""
 
-    #这边可以根据不同的参数选择不同的操作
-    #我是使用了两个文件，一个是导入一个导出
-    export(db,user,password)
+    # 这边可以根据不同的参数选择不同的操作
+    # 我是使用了两个文件，一个是导入一个导出
+    export(db, user, password)
+
 
 if __name__ == '__main__':
     main()
